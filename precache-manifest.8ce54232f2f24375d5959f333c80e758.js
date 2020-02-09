a8341f93dbc418fc82ec50c912e588bd373d7360
GlebZhidovich <gzhidovich@gmail.com>
2020-02-09 23:23:40 +0300

    feat: add files for site

---
+++ precache-manifest.8ce54232f2f24375d5959f333c80e758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9b1cb8f9ac7cf938148ae97fa02d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb4da3a5c4d17bec4f66",
+    "url": "/static/css/main.2efa1d9e.chunk.css"
+  },
+  {
+    "revision": "4628825a82aa82f2e085",
+    "url": "/static/js/2.5a22d616.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.5a22d616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb4da3a5c4d17bec4f66",
+    "url": "/static/js/main.17c0f203.chunk.js"
+  },
+  {
+    "revision": "b682bd95735f01ee5b15",
+    "url": "/static/js/runtime-main.01a645c9.js"
+  }
+]);
